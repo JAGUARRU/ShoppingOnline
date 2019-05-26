e4b2ce80c975fcabf30ac88ff758efe82b13fa45
srysgag <akkawit.tah@gmail.com>
2019-05-26 21:09:15 +0700

    init

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyCfLvunhdOlAJh9e5dHXB5jUe2SJEvLcto",
+  authDomain: "my-project-1557576876965.firebaseapp.com",
+  databaseURL: "https://my-project-1557576876965.firebaseio.com",
+  projectId: "my-project-1557576876965",
+  storageBucket: "my-project-1557576876965.appspot.com",
+  messagingSenderId: "843953801616",
+  appId: "1:843953801616:web:d28a9bf115652fc6"
+};
